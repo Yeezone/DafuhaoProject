cb3da64b30477dc744bd240e96a7978181e65841
yeezone <405821065@qq.com>
2017-07-16 11:26:07 +0800

    排行榜和返回按钮

---
+++ Assets/Scripts/ExitManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitManager : MonoBehaviour {
+    public GameObject ExitWindow;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Application.platform == RuntimePlatform.Android && (Input.GetKeyDown(KeyCode.Escape)))
+        {
+            if (!ExitWindow.activeSelf)
+            {
+                ExitWindow.SetActive(true);
+            }
+            else
+            {
+                ExitWindow.SetActive(false);
+            }
+          
+        }
+    }
+}
